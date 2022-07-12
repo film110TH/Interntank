c19de55904834ede10c8914370e30a65a0cd15bd
Karameet <61343092+film110TH@users.noreply.github.com>
2022-07-12 18:59:43 +0700

    Enemyspawn

--- Assets/Script/EnemyControoler/EnemySpawn.cs
+++ Assets/Script/EnemyControoler/EnemySpawn.cs
@@ -7,20 +7,29 @@ public class EnemySpawn : MonoBehaviour
     public GameObject[] EnemySpawnpoin;
     public GameObject[] Enemyprefab;
 
+    public GameObject[] Enemycount;
+
     public int EnemyCount = 3;
     void Start()
     {
-        
+        StartCoroutine(countSpawnEnemy());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //EnemyCount = FindObjectsOfType<Enemycontroller>();
+    }
+
+    IEnumerator countSpawnEnemy()
+    {
+        yield return new WaitForSeconds(3f);
+        spawnEnemy();
     }
 
     void spawnEnemy()
-    {
-
+    {      
+            int rand = Random.Range(0, 3);
+            GameObject enemy = Instantiate(Enemyprefab[0], EnemySpawnpoin[rand].transform.position, EnemySpawnpoin[rand].transform.rotation);     
     }
 }
